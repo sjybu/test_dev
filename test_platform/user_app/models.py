90d849e5bd8118c48f74c6f23dd5eece9268d28f
sunjunyao <14222625@qq.com>
2018-09-26 21:11:14 +0800

    实现前端登录页面

---
+++ test_platform/user_app/models.py
@@ -0,0 +1,4 @@
+from django.db import models
+
+# Create your models here.
+#ORM创建数据库表
---
+++ test_platform/user_app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# 登录请求.
+def index(request):
+    return render(request,'index.html')
+
+#处理登录请求
+def login_action(request):
+    if request.method == 'GET':
+        username = request.GET.get('username')
+        password = request.GET.get('password')
+        if username == '' or password == '':
+            return render(request,'index.html',{'error':'用户名或密码错误'})
